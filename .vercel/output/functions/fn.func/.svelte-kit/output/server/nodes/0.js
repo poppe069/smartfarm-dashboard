0bd3561e482e58158b9238b7a507410bf2019fc3
poppe069 <poppe069@gmail.com>
2023-11-17 19:57:37 +0700

    Remove .vercel

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.1269d3d4.js","_app/immutable/chunks/scheduler.c4efabc0.js","_app/immutable/chunks/index.65033d26.js","_app/immutable/chunks/index.aff71f49.js","_app/immutable/chunks/utils.7c1dc23b.js"];
-export const stylesheets = ["_app/immutable/assets/0.8b41ad48.css"];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.a8fadd5c.js","_app/immutable/chunks/scheduler.c4efabc0.js","_app/immutable/chunks/index.65033d26.js","_app/immutable/chunks/singletons.a46cf620.js","_app/immutable/chunks/index.aff71f49.js"];
-export const stylesheets = [];
-export const fonts = [];
